87fe8127e62256da750d7d02e8bbbe73b05c943f
Emmanuel Bustos <ema2159@gmail.com>
2024-04-02 22:37:02 +0200

    feat: Add basic commands to the REPL

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use std::io::{self, BufRead, Write};
 
 fn print_prompt() {
     print!("db > ");
-    io::stdout().flush();
+    io::stdout().flush().expect("Could not print prompt");
+}
+
+fn process_input(input_str: &str) {
+    if input_str == ".exit" {
+        std::process::exit(0);
+    } else {
+        println!("Unrecognized command {}", input_str);
+    }
 }
 
 fn main() {
@@ -13,6 +21,6 @@ fn main() {
         input_buffer.clear();
         io::stdin().lock().read_line(&mut input_buffer).expect("Could not read line");
 
-        println!("{}", input_buffer.trim_end());
+        process_input(input_buffer.trim_end());
     }
 }
