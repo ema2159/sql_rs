7e6f92ad84d60eec0c60d716cc07a395af5f1371
Emmanuel Bustos <ema2159@gmail.com>
2024-05-09 23:13:53 +0200

    feat: Use dialogue to manage prompt

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,38 @@
 use std::error::Error;
-use std::io::{self, BufRead, Write};
 
+use dialoguer::{theme::ColorfulTheme, BasicHistory, Input};
+
+mod entities;
 mod metacommand_processor;
 mod sql_compiler;
 mod virtual_machine;
-mod entities;
 
 use metacommand_processor::process_metacommand;
 use sql_compiler::parse_statement;
 use virtual_machine::execute_statement;
 
-fn print_prompt() -> Result<(), Box<dyn Error>> {
-    print!("db > ");
-    io::stdout().flush()?;
-    Ok(())
-}
 
 fn process_input(input_str: &str) {
     if input_str.starts_with('.') {
         process_metacommand(input_str);
-        return
+        return;
     }
     match parse_statement(input_str) {
         Ok(parsed_statement) => execute_statement(parsed_statement),
-        Err(parse_error) => println!("{}", parse_error)
+        Err(parse_error) => println!("{}", parse_error),
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input_buffer = String::new();
+    let mut history = BasicHistory::new().max_entries(8).no_duplicates(true);
 
     loop {
-        print_prompt()?;
-        input_buffer.clear();
-        io::stdin().lock().read_line(&mut input_buffer)?;
-
-        process_input(&input_buffer.trim());
+        if let Ok(input) = Input::<String>::with_theme(&ColorfulTheme::default())
+            .with_prompt("db")
+            .history_with(&mut history)
+            .interact_text()
+        {
+            process_input(input.trim());
+        }
     }
 }
