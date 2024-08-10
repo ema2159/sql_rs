3557365961a948a3b9feda50bf85bd2908eb1157
Emmanuel Bustos <ema2159@gmail.com>
2024-08-10 08:17:09 +0200

    feat: Add CLI arguments parser

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,40 @@ fn process_input(input_str: &str, db_instance: Option<&mut Database>) {
         return;
     }
     match parse_statement(input_str) {
-        Ok(parsed_statement) => println!("{:?}", VM::execute_statement(parsed_statement, db_instance)),
+        Ok(parsed_statement) => {
+            println!("{:?}", VM::execute_statement(parsed_statement, db_instance))
+        }
         Err(parse_error) => println!("{}", parse_error),
+    }
+}
+
+fn parse_args(args: Vec<String>) -> Option<Database> {
+    match args.len() {
+        1 => None,
+        2 => {
+            let db_file_name = &args[1];
+            match Database::open(db_file_name) {
+                Ok(db) => Some(db),
+                Err(err) => {
+                    eprintln!(
+                        "Cannot open database {}. Encountered the following error: {}",
+                        db_file_name, err
+                    );
+                    None
+                }
+            }
+        }
+        _ => {
+            eprintln!("Too many arguments. Please provide a single database to open.");
+            std::process::exit(1);
+        }
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-
-    // if args.len() == 1 {
-    //     db_instance = None;
-    // } else if args.len() == 2 {
-    //     let db_file_name = &args[1];
-    //     db_instance = Some(&mut Database::open(db_file_name)?)
-    // } else {
-    //     panic!("Too many arguments. Please provide a single database to open.")
-    // }
-    let db_file_name = &args[1];
-    let mut db_instance = Database::open(db_file_name)?;
+    let mut db_from_args = parse_args(args);
+    let mut db_instance = db_from_args.take();
 
     let mut prompt_history = BasicHistory::new().max_entries(8).no_duplicates(true);
 
@@ -46,7 +62,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .history_with(&mut prompt_history)
             .interact_text()
         {
-            process_input(input.trim(), Some(&mut db_instance));
+            process_input(input.trim(), db_instance.as_mut());
         }
     }
 }
