efa5a505a12fd0a2931da45e2dca6744c77f9dc3
Emmanuel Bustos <ema2159@gmail.com>
2024-08-11 20:14:06 +0200

    feat: Implement .open metacommand

--- src/main.rs
+++ src/main.rs
@@ -9,52 +9,38 @@ mod sql_compiler;
 mod virtual_machine;
 
 use backend::database::Database;
-use metacommand_processor::process_metacommand;
+use metacommand_processor::{open_metacommand, process_metacommand};
 use sql_compiler::parse_statement;
 use virtual_machine as VM;
 
 fn process_input(input_str: &str, db_instance: &mut Option<Database>) {
     if input_str.starts_with('.') {
         if let Err(metacommand_err) = process_metacommand(input_str, db_instance) {
-            println!("{}", metacommand_err)
+            eprintln!("{}", metacommand_err)
         }
         return;
     }
     match parse_statement(input_str) {
         Ok(parsed_statement) => {
             let _ = VM::execute_statement(parsed_statement, db_instance.as_mut())
-                .inspect_err(|err| println!("{}", err.to_string()));
+                .inspect_err(|err| eprintln!("{}", err.to_string()));
         }
-        Err(parse_error) => println!("{}", parse_error),
+        Err(parse_error) => eprintln!("{}", parse_error),
     }
 }
 
-fn parse_args(args: Vec<String>) -> Option<Database> {
-    match args.len() {
-        1 => None,
-        2 => {
-            let db_file_name = &args[1];
-            match Database::open(db_file_name) {
-                Ok(db) => Some(db),
-                Err(err) => {
-                    eprintln!(
-                        "Cannot open database {}. Encountered the following error: {}",
-                        db_file_name, err
-                    );
-                    None
-                }
-            }
-        }
-        _ => {
-            eprintln!("Too many arguments. Please provide a single database to open.");
-            std::process::exit(1);
-        }
+fn parse_args(db_instance: &mut Option<Database>, args: Vec<String>) {
+    if args.len() > 1 {
+        let _ = open_metacommand(db_instance, args[1..].to_vec())
+            .inspect_err(|err| eprintln!("{}", err.to_string()));
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let mut db_instance = parse_args(args);
+    let mut db_instance = None;
+
+    parse_args(&mut db_instance, args);
 
     let mut prompt_history = BasicHistory::new().max_entries(8).no_duplicates(true);
 
--- src/metacommand_processor.rs
+++ src/metacommand_processor.rs
@@ -12,33 +12,34 @@ enum Metacommand {
     Close,
     Databases,
     Exit,
+    Open,
 }
 
 #[derive(Error, Debug)]
 pub enum MetacommandErr {
     #[error("No database open.")]
     DBClosed,
-    #[error("Unrecognized Metacommand: {0}")]
-    UnrecognizedMetacommand(String),
-    #[error("Not a metacommand")]
-    NotAMetacommand,
+    #[error("Extra argument: : {0}")]
+    ExtraArgument(String),
     #[error("Error when executing .databases metacommand: {0}")]
     ListDatabasesError(String),
+    #[error("Not a metacommand")]
+    NotAMetacommand,
+    #[error("Cannot open database {0}. Encountered the following error: {1}")]
+    OpenDBError(String, String),
+    #[error("Unrecognized Metacommand: {0}")]
+    UnrecognizedMetacommand(String),
 }
 
 fn list_db_files() -> std::io::Result<()> {
-    // Get the current working directory
     let current_dir = env::current_dir()?;
 
-    // Define the file extension to search for
-    let extension = "db"; // Change this to the desired file extension
+    let extension = "db";
 
-    // Iterate over the entries in the directory
     for entry in fs::read_dir(current_dir)? {
         let entry = entry?;
         let path = entry.path();
 
-        // Check if the path is a file and if it has the desired extension
         if path.is_file() {
             if let Some(ext) = path.extension() {
                 if ext == extension {
@@ -51,7 +52,7 @@ fn list_db_files() -> std::io::Result<()> {
     Ok(())
 }
 
-fn close_database(db_instance: &mut Option<Database>) -> Result<(), MetacommandErr> {
+fn close_metacommand(db_instance: &mut Option<Database>) -> Result<(), MetacommandErr> {
     match db_instance {
         Some(db) => {
             db.close();
@@ -72,6 +73,31 @@ fn databases_metacommand() -> Result<(), MetacommandErr> {
     }
 }
 
+fn exit_metacommand(db_instance: &mut Option<Database>) -> ! {
+    let _ = close_metacommand(db_instance);
+    std::process::exit(SUCCESS)
+}
+
+pub fn open_metacommand(
+    db_instance: &mut Option<Database>,
+    args: Vec<String>,
+) -> Result<(), MetacommandErr> {
+    if args.len() > 1 {
+        return Err(MetacommandErr::ExtraArgument(args[1].to_string()));
+    }
+
+    let _ = close_metacommand(db_instance);
+
+    let db_filename = &args[0];
+
+    *db_instance =
+        Some(Database::open(db_filename).map_err(|err| {
+            MetacommandErr::OpenDBError(db_filename.to_string(), err.to_string())
+        })?);
+
+    Ok(())
+}
+
 impl FromStr for Metacommand {
     type Err = MetacommandErr;
 
@@ -80,20 +106,28 @@ impl FromStr for Metacommand {
             "close" => Ok(Metacommand::Close),
             "databases" => Ok(Metacommand::Databases),
             "exit" => Ok(Metacommand::Exit),
+            "open" => Ok(Metacommand::Open),
             _ => Err(MetacommandErr::UnrecognizedMetacommand(s.to_string())),
         }
     }
 }
 
 pub fn process_metacommand(
-    metacommand_str: &str,
+    input_str: &str,
     db_instance: &mut Option<Database>,
 ) -> Result<(), MetacommandErr> {
+    let tokens: Vec<_> = input_str.split(" ").collect();
+
+    // input_str cannot be empty, so unrwap is ok
+    let (metacommand_str, args) = tokens.split_first().unwrap();
     let metacommand = Metacommand::from_str(metacommand_str)?;
 
     match metacommand {
-        Metacommand::Close => close_database(db_instance),
+        Metacommand::Close => close_metacommand(db_instance),
         Metacommand::Databases => databases_metacommand(),
-        Metacommand::Exit => std::process::exit(SUCCESS),
+        Metacommand::Exit => exit_metacommand(db_instance),
+        Metacommand::Open => {
+            open_metacommand(db_instance, args.iter().map(|s| s.to_string()).collect())
+        }
     }
 }
